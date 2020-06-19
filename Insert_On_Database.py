94e95d73fc2534af4eca948da88efcf6cfdcaef7
premjogu98 <premjogu98@gmail.com>
2020-06-19 19:29:33 +0530

    add  new connection

--- Insert_On_Database.py
+++ Insert_On_Database.py
@@ -9,7 +9,7 @@ import wx
 app = wx.App()
 
 
-def Local_connection():
+def DB_connection():
     mydb_Local = ''
     a = 0
     while a == 0:
@@ -41,38 +41,38 @@ def Local_connection():
             time.sleep(10)
 
 
-def L2L_connection():
-    mydb_L2L = ''
-    a3 = 0
-    while a3 == 0:
-        try:
-            File_Location = open("D:\\0 PYTHON EXE SQL CONNECTION & DRIVER PATH\\sberbank-ast.ru\\Location For Database & Driver.txt" , "r")
-            TXT_File_AllText = File_Location.read()
+# def L2L_connection():
+#     mydb_L2L = ''
+#     a3 = 0
+#     while a3 == 0:
+#         try:
+#             File_Location = open("D:\\0 PYTHON EXE SQL CONNECTION & DRIVER PATH\\sberbank-ast.ru\\Location For Database & Driver.txt" , "r")
+#             TXT_File_AllText = File_Location.read()
 
-            L2L_host = str(TXT_File_AllText).partition("L2L_host=")[2].partition(",")[0].strip()
-            L2L_user = str(TXT_File_AllText).partition("L2L_user=")[2].partition(",")[0].strip()
-            L2L_password = str(TXT_File_AllText).partition("L2L_password=")[2].partition(",")[0].strip()
-            L2L_db = str(TXT_File_AllText).partition("L2L_db=")[2].partition(",")[0].strip()
-            L2L_charset = str(TXT_File_AllText).partition("L2L_charset=")[2].partition("\")")[0].strip()
+#             L2L_host = str(TXT_File_AllText).partition("L2L_host=")[2].partition(",")[0].strip()
+#             L2L_user = str(TXT_File_AllText).partition("L2L_user=")[2].partition(",")[0].strip()
+#             L2L_password = str(TXT_File_AllText).partition("L2L_password=")[2].partition(",")[0].strip()
+#             L2L_db = str(TXT_File_AllText).partition("L2L_db=")[2].partition(",")[0].strip()
+#             L2L_charset = str(TXT_File_AllText).partition("L2L_charset=")[2].partition("\")")[0].strip()
 
-            mydb_L2L = pymysql.connect(host=str(L2L_host),
-                                         user=str(L2L_user),
-                                         password=str(L2L_password),
-                                         db=str(L2L_db),
-                                         charset=str(L2L_charset),
-                                         cursorclass=pymysql.cursors.DictCursor)
+#             mydb_L2L = pymysql.connect(host=str(L2L_host),
+#                                          user=str(L2L_user),
+#                                          password=str(L2L_password),
+#                                          db=str(L2L_db),
+#                                          charset=str(L2L_charset),
+#                                          cursorclass=pymysql.cursors.DictCursor)
 
-            a3 = 1
-            return mydb_L2L
-        except pymysql.connect as e:
-            wx.MessageBox('Error On L2L_connection Function', 'sberbank_ast.ru', wx.OK | wx.ICON_ERROR)
-            exc_type , exc_obj , exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print("Error ON : " , sys._getframe().f_code.co_name + "--> " + str(e) , "\n" , exc_type , "\n" , fname ,
-                  "\n" , exc_tb.tb_lineno)
-            time.sleep(10)
-            mydb_L2L.close()
-            a3 = 0
+#             a3 = 1
+#             return mydb_L2L
+#         except pymysql.connect as e:
+#             wx.MessageBox('Error On L2L_connection Function', 'sberbank_ast.ru', wx.OK | wx.ICON_ERROR)
+#             exc_type , exc_obj , exc_tb = sys.exc_info()
+#             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+#             print("Error ON : " , sys._getframe().f_code.co_name + "--> " + str(e) , "\n" , exc_type , "\n" , fname ,
+#                   "\n" , exc_tb.tb_lineno)
+#             time.sleep(10)
+#             mydb_L2L.close()
+#             a3 = 0
 
 
 def check_Duplication(get_htmlSource, SegFeild,Hidden_Script,get_htmlsource_For_XML_DATA):
@@ -80,16 +80,16 @@ def check_Duplication(get_htmlSource, SegFeild,Hidden_Script,get_htmlsource_For_
     a1 = 0
     while a1 == 0:
         try:
-            mydb_Local = Local_connection()
+            mydb_Local = DB_connection()
             mycursorLocal = mydb_Local.cursor()
             if SegFeild[13] != '' and SegFeild[24] != '' and SegFeild[7] != '':
-                commandText = "SELECT Posting_Id from Tenders where tender_notice_no = '" + str(SegFeild[13]) + "' and Country = '" + str(SegFeild[7]) + "' and doc_last= '" + str(SegFeild[24]) + "'"
+                commandText = "SELECT Posting_Id from russia_tenders_tbl where tender_notice_no = '" + str(SegFeild[13]) + "' and Country = '" + str(SegFeild[7]) + "' and doc_last= '" + str(SegFeild[24]) + "'"
             elif SegFeild[13] != "" and SegFeild[7] != "":
-                commandText = "SELECT Posting_Id from Tenders where tender_notice_no = '" + str(SegFeild[13]) + "' and Country = '" + str(SegFeild[7]) + "'"
+                commandText = "SELECT Posting_Id from russia_tenders_tbl where tender_notice_no = '" + str(SegFeild[13]) + "' and Country = '" + str(SegFeild[7]) + "'"
             elif SegFeild[19] != "" and SegFeild[24] != "" and SegFeild[7] != "":
-                commandText = "SELECT Posting_Id from Tenders where short_desc = '" + str(SegFeild[19]) + "' and doc_last = '" + SegFeild[24] + "' and Country = '" + SegFeild[7] + "'"
+                commandText = "SELECT Posting_Id from russia_tenders_tbl where short_desc = '" + str(SegFeild[19]) + "' and doc_last = '" + SegFeild[24] + "' and Country = '" + SegFeild[7] + "'"
             else:
-                commandText = "SELECT Posting_Id from Tenders where short_desc = '" + str(SegFeild[19]) + "' and Country = '" + str(SegFeild[7]) + "'"
+                commandText = "SELECT Posting_Id from russia_tenders_tbl where short_desc = '" + str(SegFeild[19]) + "' and Country = '" + str(SegFeild[7]) + "'"
             mycursorLocal.execute(commandText)
             results = mycursorLocal.fetchall()
             mydb_Local.close()
@@ -99,11 +99,11 @@ def check_Duplication(get_htmlSource, SegFeild,Hidden_Script,get_htmlsource_For_
             print("Code Reached On check_Duplication")
             return results
         except Exception as e:
-            mydb_L2L = Local_connection()
+            mydb_L2L = DB_connection()
             mycursorL2L = mydb_L2L.cursor()
             Function_name: str = sys._getframe().f_code.co_name
             Error: str = str(e)
-            sql1 = "INSERT INTO ErrorLog(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'" , "") + "','" + str(Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
+            sql1 = "INSERT INTO errorlog_tbl(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'" , "") + "','" + str(Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
             mycursorL2L.execute(sql1)
             mydb_L2L.commit()
             exc_type , exc_obj , exc_tb = sys.exc_info()
@@ -124,9 +124,9 @@ def insert_in_Local(get_htmlSource , SegFeild,Hidden_Script,get_htmlsource_For_X
         Fileid = create_filename(get_htmlSource , SegFeild,Hidden_Script,get_htmlsource_For_XML_DATA)
     MyLoop = 0
     while MyLoop == 0:
-        mydb_Local = Local_connection()
+        mydb_Local = DB_connection()
         mycursorLocal = mydb_Local.cursor()
-        sql = "INSERT INTO Tenders(EMail,add1,Country,Maj_Org,tender_notice_no,notice_type,Tenders_details,short_desc,est_cost,currency,doc_cost,doc_last,earnest_money,Financier,tender_doc_file,source)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        sql = "INSERT INTO russia_tenders_tbl(EMail,add1,Country,Maj_Org,tender_notice_no,notice_type,Tenders_details,short_desc,est_cost,currency,doc_cost,doc_last,earnest_money,Financier,tender_doc_file,source)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         val= (str(SegFeild[1]) , str(SegFeild[2]) , str(SegFeild[7]) , str(SegFeild[12]) , str(SegFeild[13]) , str(SegFeild[14]),
                 str(SegFeild[18]) , str(SegFeild[19]) , str(SegFeild[20]) , str(SegFeild[21]) , str(SegFeild[22]), str(SegFeild[24]),str(SegFeild[26]) ,str(SegFeild[27]),
                 str(SegFeild[28]) , str(SegFeild[31]))
@@ -139,11 +139,11 @@ def insert_in_Local(get_htmlSource , SegFeild,Hidden_Script,get_htmlsource_For_X
             print("Code Reached On insert_in_Local")
             MyLoop = 1
         except Exception as e:
-            mydb_L2L = L2L_connection()
+            mydb_L2L = DB_connection()
             mycursorL2L = mydb_L2L.cursor()
             Function_name :str = sys._getframe().f_code.co_name
             Error : str = str(e)
-            sql1 = "INSERT INTO ErrorLog(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'","") + "','" + str(Function_name).replace("'","")+ "','"+str(SegFeild[31])+"')"
+            sql1 = "INSERT INTO errorlog_tbl(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'","") + "','" + str(Function_name).replace("'","")+ "','"+str(SegFeild[31])+"')"
             mycursorL2L.execute(sql1)
             mydb_L2L.commit()
             exc_type , exc_obj , exc_tb = sys.exc_info()
@@ -193,11 +193,11 @@ def create_filename(get_htmlSource , SegFeild,Hidden_Script,get_htmlsource_For_X
             print("Code Reached On create_filename")
             return Fileid
         except Exception as e:
-            mydb_L2L = L2L_connection()
+            mydb_L2L = DB_connection()
             mycursorL2L = mydb_L2L.cursor()
             Function_name: str = sys._getframe().f_code.co_name
             Error: str = str(e)
-            sql1 = "INSERT INTO ErrorLog(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'", "") + "','" + str(
+            sql1 = "INSERT INTO errorlog_tbl(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'", "") + "','" + str(
                 Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
             mycursorL2L.execute(sql1)
             mydb_L2L.commit()
@@ -245,19 +245,19 @@ def insert_L2L(SegFeild , Fileid):
     dms_downloadfiles_tbldatatype = "A"
     dms_entrynotice_tblnotice_type = '2'
     file_id = Fileid
-    mydb_L2L = L2L_connection()
+    mydb_L2L = DB_connection()
     mycursorL2L = mydb_L2L.cursor()
     if SegFeild[12] != "" and SegFeild[19] != "" and SegFeild[24] != "" and SegFeild[7] != "" and SegFeild[2] != "":
         dms_entrynotice_tblcompulsary_qc = "2"
     else:
         dms_entrynotice_tblcompulsary_qc = "1"
         Global_var.QC_Tender += 1
-        sql = "INSERT INTO QCTenders(Source,tender_notice_no,short_desc,doc_last,Maj_Org,Address,doc_path,Country)VALUES(%s,%s,%s,%s,%s,%s,%s,%s) "
+        sql = "INSERT INTO qctenders_tbl(Source,tender_notice_no,short_desc,doc_last,Maj_Org,Address,doc_path,Country)VALUES(%s,%s,%s,%s,%s,%s,%s,%s) "
         val = (str(SegFeild[31]) , str(SegFeild[13]) , str(SegFeild[19]) , str(SegFeild[24]) , str(SegFeild[12]) ,str(SegFeild[2]) , "http://tottestupload3.s3.amazonaws.com/" + file_id + ".html" , str(SegFeild[7]))
         a4 = 0
         while a4 == 0:
             try:
-                mydb_L2L = L2L_connection()
+                mydb_L2L = DB_connection()
                 mycursorL2L = mydb_L2L.cursor()
                 mycursorL2L.execute(sql , val)
                 mydb_L2L.commit()
@@ -266,7 +266,7 @@ def insert_L2L(SegFeild , Fileid):
             except Exception as e:
                 Function_name: str = sys._getframe().f_code.co_name
                 Error: str = e
-                sql1 = "INSERT INTO ErrorLog(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'" , "") + "','" + str(Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
+                sql1 = "INSERT INTO errorlog_tbl(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'" , "") + "','" + str(Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
                 mycursorL2L.execute(sql1)
                 mydb_L2L.commit()
                 exc_type , exc_obj , exc_tb = sys.exc_info()
@@ -275,12 +275,12 @@ def insert_L2L(SegFeild , Fileid):
                 a4 = 0
                 time.sleep(10)
 
-    sql = "INSERT INTO Final_Tenders(notice_no,file_id,purchaser_name,deadline,country,description,purchaser_address,purchaser_email,purchaser_url,purchaser_emd,purchaser_value,financier,deadline_two,tender_details,ncbicb,status,added_on,search_id,cpv_value,cpv_userid,quality_status,quality_id,quality_addeddate,source,tender_doc_file,Col1,Col2,Col3,Col4,Col5,file_name,user_id,status_download_id,save_status,selector_id,select_date,datatype,compulsary_qc,notice_type,cqc_status,DocCost,DocLastDate,is_english)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) "
+    sql = "INSERT INTO l2l_tenders_tbl(notice_no,file_id,purchaser_name,deadline,country,description,purchaser_address,purchaser_email,purchaser_url,purchaser_emd,purchaser_value,financier,deadline_two,tender_details,ncbicb,status,added_on,search_id,cpv_value,cpv_userid,quality_status,quality_id,quality_addeddate,source,tender_doc_file,Col1,Col2,Col3,Col4,Col5,file_name,user_id,status_download_id,save_status,selector_id,select_date,datatype,compulsary_qc,notice_type,cqc_status,DocCost,DocLastDate,is_english)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) "
     val = (str(SegFeild[13]) , file_id , str(SegFeild[12]) , str(SegFeild[24]) , str(SegFeild[7]) , str(SegFeild[19]) ,str(SegFeild[2]) ,str(SegFeild[1]) , str(SegFeild[8]) , str(SegFeild[26]) , str(SegFeild[20]) , str(SegFeild[27]) ,str(SegFeild[24]) , str(SegFeild[18]) , ncb_icb , dms_entrynotice_tblstatus , str(added_on) , search_id ,str(SegFeild[36]) ,cpv_userid , dms_entrynotice_tblquality_status , quality_id , str(quality_addeddate) , str(SegFeild[31]) ,str(SegFeild[28]) ,Col1 , Col2 , Col3 , Col4 , Col5 ,file_name , dms_downloadfiles_tbluser_id , dms_downloadfiles_tblstatus , dms_downloadfiles_tblsave_status ,selector_id , str(select_date) , dms_downloadfiles_tbldatatype ,dms_entrynotice_tblcompulsary_qc , dms_entrynotice_tblnotice_type , dms_entrynotice_tbl_cqc_status ,str(SegFeild[22]) , str(SegFeild[41]),str(is_english))
     a5 = 0
     while a5 == 0:
         try:
-            mydb_L2L = L2L_connection()
+            mydb_L2L = DB_connection()
             mycursorL2L = mydb_L2L.cursor()
             mycursorL2L.execute(sql , val)
             mydb_L2L.commit()
@@ -292,8 +292,8 @@ def insert_L2L(SegFeild , Fileid):
         except Exception as e:
             Function_name: str = sys._getframe().f_code.co_name
             Error: str = str(e)
-            sql1 = "INSERT INTO ErrorLog(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'" , "") + "','" + str(Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
-            mydb_L2L = L2L_connection()
+            sql1 = "INSERT INTO errorlog_tbl(Error_Message,Function_Name,Exe_Name) VALUES('" + str(Error).replace("'" , "") + "','" + str(Function_name).replace("'" , "") + "','" + str(SegFeild[31]) + "')"
+            mydb_L2L = DB_connection()
             mycursorL2L = mydb_L2L.cursor()
             mycursorL2L.execute(sql1)
             mydb_L2L.commit()
